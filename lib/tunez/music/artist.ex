34e6cd8a87d837b58b784d457ad70dc23390a58b
zwpdbh <hyperion_z@outlook.com>
2025-06-12 21:58:21 +0800

    > Defining a change module

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -16,7 +16,24 @@ defmodule Tunez.Music.Artist do
     end
 
     update :update do
+      require_atomic? false
+
       accept [:name, :biography]
+
+      # TODO: rewrite change atomically
+      change fn changeset, _context ->
+               new_name = Ash.Changeset.get_attribute(changeset, :name)
+               previous_name = Ash.Changeset.get_data(changeset, :name)
+               previous_names = Ash.Changeset.get_data(changeset, :previous_names)
+
+               names =
+                 [previous_name | previous_names]
+                 |> Enum.uniq()
+                 |> Enum.reject(fn name -> name == new_name end)
+
+               Ash.Changeset.change_attribute(changeset, :previous_names, names)
+             end,
+             where: [changing(:name)]
     end
 
     destroy :destroy do
@@ -31,6 +48,10 @@ defmodule Tunez.Music.Artist do
     end
 
     attribute :biography, :string
+
+    attribute :previous_names, {:array, :string} do
+      default []
+    end
 
     timestamps()
   end
