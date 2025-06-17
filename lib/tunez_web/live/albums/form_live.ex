b7597b0dc6e6ae19b8a6e0c3a484dcdb22eca7d5
zwpdbh <hyperion_z@outlook.com>
2025-06-17 22:56:05 +0800

    > Automatic Conversions Between Seconds and Minutes

--- lib/tunez_web/live/albums/form_live.ex
+++ lib/tunez_web/live/albums/form_live.ex
@@ -87,7 +87,7 @@ defmodule TunezWeb.Albums.FormLive do
         <.inputs_for :let={track_form} field={@form[:tracks]}>
           <tr data-id={track_form.index}>
             <td class="px-3 w-20">
-              <%!-- <.input field={track_form[:order]} type="number" /> --%>
+              <span class="hero-bars-3 handle cursor-pointer" />
             </td>
             <td class="px-3">
               <label for={track_form[:name].id} class="hidden">Name</label>
@@ -164,7 +164,10 @@ defmodule TunezWeb.Albums.FormLive do
     {:noreply, socket}
   end
 
-  def handle_event("reorder-tracks", %{"order" => _order}, socket) do
+  def handle_event("reorder-tracks", %{"order" => order}, socket) do
+    socket =
+      update(socket, :form, fn form -> AshPhoenix.Form.sort_forms(form, [:tracks], order) end)
+
     {:noreply, socket}
   end
 end
